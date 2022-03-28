f128f24f18090a6d8071cff5aa48f13c9704e0d8
Chriswihu <99390466+Chriswihu@users.noreply.github.com>
2022-03-28 11:36:22 +0200

    init commit

--- src/com/company/DieselTruck.java
+++ src/com/company/DieselTruck.java
@@ -2,4 +2,7 @@ package com.company;
 
 public class DieselTruck extends DieselCar
 {
+    protected DieselTruck(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre, boolean hasFilter) {
+        super(registrationNumber, make, model, numberOfDoors, kmPrLitre, hasFilter);
+    }
 }
